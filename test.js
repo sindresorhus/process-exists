8b7bb6f1292b6fe0c24b96861fcde820dd167595
Kevin Martensson <kevinmartensson@gmail.com>
2015-10-16 18:25:05 +0200

    Use promises

--- test.js
+++ test.js
@@ -1,37 +1,32 @@
 'use strict';
 var test = require('ava');
 var noopProcess = require('noop-process');
+var pify = require('pify');
 var processExists = require('./');
 
 test('pid', function (t) {
-	t.plan(4);
+	t.plan(2);
 
-	processExists(process.pid, function (err, exists) {
-		t.assert(!err, err);
+	processExists(process.pid).then(function (exists) {
 		t.assert(exists);
 	});
 
-	processExists(345234531, function (err, exists) {
-		t.assert(!err, err);
+	processExists(345234531).then(function (exists) {
 		t.assert(!exists);
 	});
 });
 
 test('title', function (t) {
-	t.plan(5);
+	t.plan(2);
 
 	var title = 'pe-test';
 
-	noopProcess({title: title}, function (err) {
-		t.assert(!err, err);
-
-		processExists(title, function (err, exists) {
-			t.assert(!err, err);
+	pify(noopProcess)({title: title}).then(function () {
+		processExists(title).then(function (exists) {
 			t.assert(exists);
 		});
 
-		processExists('pe-unicorn', function (err, exists) {
-			t.assert(!err, err);
+		processExists('pe-unicorn').then(function (exists) {
 			t.assert(!exists);
 		});
 	});
