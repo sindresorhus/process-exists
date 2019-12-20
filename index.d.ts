93c291566fa414c7c8b8965f06d8c00191f7dc12
Gary King <garyking@gmail.com>
2019-12-20 17:50:02 -0500

    declare const

--- index.d.ts
+++ index.d.ts
@@ -1,21 +1,19 @@
-/**
-Check if a process exists.
+declare const processExists: {
+	/**
+	Check if a process exists.
 
-@param input - Process ID or name to check.
-@returns - The process exists.
-*/
-declare function processExists(input: number | string): Promise<boolean>;
+	@param input - Process ID or name to check.
+	@returns - The process exists.
+	*/
+	(input: number | string): Promise<boolean>;
 
-declare namespace processExists {
 	/**
 	Check multiple processes if they exist.
 
 	@param input - Process IDs or names to check.
 	@returns - A map, with the process name/ID as key and the status as a boolean value.
 	*/
-	function all(
-		input: (number | string)[],
-	): Promise<Map<number | string, boolean>>;
+	all(input: (number | string)[]): Promise<Map<number | string, boolean>>;
 
 	/**
 	Filter for processes that exist.
@@ -23,7 +21,7 @@ declare namespace processExists {
 	@param input - Process IDs or names to check.
 	@returns - The processes that exist.
 	*/
-	function filterExists(input: (number | string)[]): (number | string)[];
-}
+	filterExists(input: (number | string)[]): (number | string)[];
+};
 
 export = processExists;
