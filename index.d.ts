d69378cea39c1c4287be91ee902c7b620ae950b7
Sindre Sorhus <sindresorhus@gmail.com>
2019-12-22 18:10:41 +0100

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -3,7 +3,7 @@ declare const processExists: {
 	Check if a process exists.
 
 	@param input - Process ID or name to check.
-	@returns - The process exists.
+	@returns Whether the process exists.
 	*/
 	(input: number | string): Promise<boolean>;
 
@@ -11,7 +11,7 @@ declare const processExists: {
 	Check multiple processes if they exist.
 
 	@param input - Process IDs or names to check.
-	@returns - A map, with the process name/ID as key and the status as a boolean value.
+	@returns A map with the process name/ID as key and the status as a boolean value.
 	*/
 	all(
 		input: readonly (number | string)[],
@@ -21,7 +21,7 @@ declare const processExists: {
 	Filter for processes that exist.
 
 	@param input - Process IDs or names to check.
-	@returns - The processes that exist.
+	@returns The processes that exist.
 	*/
 	filterExists(input: readonly (number | string)[]): (number | string)[];
 };
