b254df5716e6fedf4f71634c2455fc6aedc61940
Sindre Sorhus <sindresorhus@gmail.com>
2019-12-22 18:11:13 +0100

    Update index.test-d.ts

--- index.test-d.ts
+++ index.test-d.ts
@@ -5,7 +5,7 @@ expectType<Promise<boolean>>(processExists(123));
 expectType<Promise<boolean>>(processExists('abc'));
 
 expectType<Promise<Map<number | string, boolean>>>(
-	processExists.all(['abc', 123]),
+	processExists.all(['abc', 123])
 );
 
 expectType<(number | string)[]>(processExists.filterExists(['abc', 123]));
