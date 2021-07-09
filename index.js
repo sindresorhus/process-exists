89ec3ad90766346121455f5d7d0e925e391f882a
Yanis Benson <yanis.benson@gmail.com>
2021-07-09 09:37:25 +0300

    Make tests for both name and cmd to make it more semantically correct on Linux (#10)

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 'use strict';
 const psList = require('ps-list');
 
-const fn = (wantedProcessName, process) => {
+const linuxProcessMatchesName = (wantedProcessName, process) => {
+	if (typeof wantedProcessName === 'string') {
+		return process.name === wantedProcessName || process.cmd.split(' ')[0] === wantedProcessName;
+	}
+
+	return process.pid === wantedProcessName;
+};
+
+const nonLinuxProcessMatchesName = (wantedProcessName, process) => {
 	if (typeof wantedProcessName === 'string') {
 		return process.name === wantedProcessName;
 	}
@@ -9,17 +17,19 @@ const fn = (wantedProcessName, process) => {
 	return process.pid === wantedProcessName;
 };
 
+const processMatchesName = process.platform === 'linux' ? linuxProcessMatchesName : nonLinuxProcessMatchesName;
+
 module.exports = async processName => {
 	const processes = await psList();
-	return processes.some(x => fn(processName, x));
+	return processes.some(x => processMatchesName(processName, x));
 };
 
 module.exports.all = async processName => {
 	const processes = await psList();
-	return new Map(processName.map(x => [x, processes.some(y => fn(x, y))]));
+	return new Map(processName.map(x => [x, processes.some(y => processMatchesName(x, y))]));
 };
 
 module.exports.filterExists = async processNames => {
 	const processes = await psList();
-	return processNames.filter(x => processes.some(y => fn(x, y)));
+	return processNames.filter(x => processes.some(y => processMatchesName(x, y)));
 };
